b9ea3b247aeb186cd8ae266b3d10afbfb35e71c5
Liang LIn <lianglin@outlook.com>
2017-02-17 02:25:13 +0000

    add material ui

--- app/modules/App.js
+++ app/modules/App.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router'
 
-export default React.createClass({
+import RaisedButton from 'material-ui/RaisedButton';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+  };
   render() {
     return(
-      <div>
-        <h1>Hello, React Router!</h1>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div style={styles.container}>
+          <h1>Timesheet App</h1>
+          <RaisedButton  label="Save"  secondary={true} />
+        </div>
+      </MuiThemeProvider>
     ) 
   }
-})
+}
+
+export default App;
